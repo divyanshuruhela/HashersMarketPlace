4a64b33f1f032d37fcb32ed5d645c4161a0ba5d7
druhela <druhela@deloitte.com>
2024-08-21 20:06:09 +0530

    updating code for images

--- src/app/items-service.ts
+++ src/app/items-service.ts
@@ -1,43 +1,43 @@
 
 export const DUMMY_ITEMS :  Item[] = [
     {
-      id: 'u1',
-      name: 'Jasmine Washington',
-      description: 'test',
-      price : 1000,
-      image: 'user-1.jpg',
+      id: '12',
+      name: 'SG Cricket Bat',
+      description: 'SG Thunder Plus Short Handle Kashmir Willow Cricket Bat  (990 g)',
+      price : 1200,
+      image: 'cricketbat.jpg',
       userId: '1'
     },
     {
-      id: 'u2',
-      name: 'Emily Thompson',
-      description: 'test',
-      price : 1000,
-      image: 'user-2.jpg',
+      id: '13',
+      name: 'Boult Z40 Pro',
+      description: 'Boult Z40 Pro with 100H Battery, Quad Mic ENC, Scratch Proof, Rubber Grip Design, 5.3v Bluetooth Headset',
+      price : 1300,
+      image: 'earbuds.jpg',
       userId: '2'
     },
     {
-      id: 'u3',
-      name: 'Marcus Johnson',
-      description: 'test',
-      price : 1000,
-      image: 'user-3.jpg',
+      id: '14',
+      name: 'Blue Star 2023 Model 1.5 Ton AC',
+      description: 'Blue Star 2023 Model 1.5 Ton 3 Star Split Inverter AC with Wi-fi Connect - White',
+      price : 25000,
+      image: 'AC.jpg',
       userId: '2'
     },
     {
-      id: 'u4',
-      name: 'David Miller',
-      description: 'test',
-      price : 1000,
-      image: 'user-4.jpg',
+      id: '15',
+      name: 'HP Laptop',
+      description: 'AMD Ryzen 3 Quad Core 5300U - (8 GB/512 GB SSD/Windows 11 Home) 15s-eq2143au Thin and Light Laptop',
+      price : 20000,
+      image: 'laptop.jpeg',
       userId: '2'
     },
     {
-      id: 'u5',
-      name: 'Priya Patel',
-      description: 'test',
-      price : 1000,
-      image: 'user-5.jpg',
+      id: '16',
+      name: 'Mi TV',
+      description: 'Mi A series 108 cm (43 inch) Full HD LED Smart Google TV with FHD',
+      price : 15000,
+      image: 'LedTv.jpg',
       userId: '1'
     }
   ];
